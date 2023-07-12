abcaed7017bd3fd7e24ee78ad59d36a1ecc9d6ec
Linaboygethub <linasteve5831@gmail.com>
2023-07-12 16:04:20 +0700

    Changes

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+   const date = new Date();
+   const hour = date.getHours();
+   let message='';
+   if(hour<=24){
+       message="Good Morning";
+
+   }
+   if(hour<=24){
+       message="Good Afternoon";
+   }
+   if(hour>=17){
+       message="Good Everning"
+   }
+  $('#headingText').empty().append(message+ "  From Korb Lina")
+})
